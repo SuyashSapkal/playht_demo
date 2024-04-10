cfea6e0a8066249989858099c6e82bfb861f08a9
Suyash Sudhakar Sapkal <suyash.sapkal@prdcinfotech.com>
2024-04-10 14:36:41 +0530

    changes done in FeatureContent.jsx and FeatureContent.css

--- frontend/src/components/FeatureContent.jsx
+++ frontend/src/components/FeatureContent.jsx
@@ -6,15 +6,16 @@ export default function FeatureContent(){
         <div className="featureContent">
             <img className='wave2' src={wave2} />
             <div className='feature'>
+                    <div className="h1">PlayHT Studio</div>
                 <div className='featureText'>
-                    <div className="h1">Major Features</div>
                     <div className="featurelist">
                         <p className='list'>Ultra-realistic voice generation</p>
                         <p className='list'>Text to Speech Conversion</p>
                         <p className='list'>Voice Cloning</p>
-                        <p className='list'>AI Voice Agents</p>
+                        <p className='list'>Generate AI Voice Agents</p>
                         <p className='list'>Supports Multiple Languages and Accents</p>
                     </div>
+                    <img className='studio_img' src='https://play.ht/assets/img/feature-image.webp?w=1200&q=75'></img>
                 </div>
                 <img src="" alt="" />
             </div>
