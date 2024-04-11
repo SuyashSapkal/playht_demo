398ab69e9852f05b245e18c19ed5e51d53308432
Suyash Sudhakar Sapkal <suyash.sapkal@prdcinfotech.com>
2024-04-11 21:58:07 +0530

    Changes done in Footer.jsx and Footer.css files

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,11 +3,17 @@ import './Footer.css';
 export default function Footer(){
     return(
         <div className="footer">
-            <p className="web_link" href="https://play.ht/">Official Website: play.ht</p>
-            <div className="note">
-                Note: This is not an official website. This is just a test mockup
+            <div className='item'>
+                <p className="web_link" href="https://play.ht/">Official Website: play.ht</p>
+                <div className="note">
+                    Note: This is not an official website. This is just a test mockup
+                </div>
+                <div className="email">creator email: suyashsapkal21@gmail.com</div>
             </div>
-            <div className="email">creator email: suyashsapkal21@gmail.com</div>
+            <div className='item'>
+                For more information 
+                <span className="web_link" href="https://play.ht/contact-us/"> Contact Us</span>
+            </div>
         </div>
     )
 }
